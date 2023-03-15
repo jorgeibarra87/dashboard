cdfc430f1ad5c7613d9cffc9abfca25624a41893
jorgeibarra87 <jorgeibarra87@gmail.com>
2023-03-15 18:47:33 -0500

    add components header and dropdoens functions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,161 @@
-import React from 'react'
+/** @format */
+
+import React from "react"
+import {
+  RiNotification3Line,
+  RiArrowDownSLine,
+  RiSettings3Line,
+  RiLogoutCircleRLine,
+  RiThumbUpLine,
+  RiChat3Line,
+} from "react-icons/ri"
+import { Menu, MenuItem, MenuButton, SubMenu } from "@szhsin/react-menu"
+import "@szhsin/react-menu/dist/index.css"
+import "@szhsin/react-menu/dist/transitions/slide.css"
+import { Link } from "react-router-dom"
 
 export default function Header() {
-    return (
-        <header className=' h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8'>Header</header>
-    )
+  return (
+    <header className=' h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end'>
+      <nav className='flex items-center gap-2'>
+        <Menu
+          menuButton={
+            <MenuButton className='relative hover:bg-secondary-100 p-2 rounded-lg transition-colors'>
+              <RiNotification3Line className='' />
+              <span className='absolute -top-0.5 -right-0 bg-primary py-0.5 px-[5px] text-black rounded-full text-[8px] font-bold'>
+                2
+              </span>
+            </MenuButton>
+          }
+          transition
+          align='end'
+          arrow
+          arrowClassName='bg-secondary-100'
+          menuClassName='bg-secondary-100 p-4'
+        >
+          <h1 className='text-gray-300 text-center font-medium'>
+            Notifications (2)
+          </h1>
+          <hr className='my-6 border-gray-500' />
+          <MenuItem className='p-0 hover:bg-transparent'>
+            <Link
+              to='/'
+              className='text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg'
+            >
+              <img
+                src='https://img.freepik.com/foto-gratis/negocio-hombre-negocios-exitoso-trabajando-ordenador-portatil-usando-computadora-sonriendo-pie_1258-26447.jpg'
+                alt=''
+                className='w-8 h-8 object-cover rounded-full'
+              />
+              <div className='text-sm flex flex-col'>
+                <div className='flex items-center justify-between gap-4'>
+                  <span>Jorge Ibarra</span>
+                  <span className='text-[8px]'>15/03/2023</span>
+                </div>
+                <p className='text-gray-500 text-xs'>
+                  Lorem ipsum dolor sit amet...
+                </p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className='p-0 hover:bg-transparent'>
+            <Link
+              to='/'
+              className='text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg'
+            >
+              <RiThumbUpLine className='p-2 bg-blue-200 text-blue-700 box-content rounded-full' />
+              <div className='text-sm flex flex-col'>
+                <div className='flex items-center justify-between gap-4'>
+                  <span>New like</span>
+                  <span className='text-[8px]'>15/03/2023</span>
+                </div>
+                <p className='text-gray-500 text-xs'>
+                  Jorge Ibarra likes your post...
+                </p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className='p-0 hover:bg-transparent'>
+            <Link
+              to='/'
+              className='text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg'
+            >
+              <RiChat3Line className='p-2 bg-yellow-200 text-yellow-700 box-content rounded-full' />
+              <div className='text-sm flex flex-col'>
+                <div className='flex items-center justify-between gap-4'>
+                  <span>New comment</span>{" "}
+                  <span className='text-[8px]'>15/03/2023</span>
+                </div>
+                <p className='text-gray-500 text-xs'>
+                  Jorge Ibarra has commented...
+                </p>
+              </div>
+            </Link>
+          </MenuItem>
+          <hr className='my-6 border-gray-500' />
+          <MenuItem className='p-0 hover:bg-transparent flex justify-center cursor-default'>
+            <Link
+              to='/'
+              className='text-gray-400 text-sm hover:text-white transition-colors'
+            >
+              All notifications
+            </Link>
+          </MenuItem>
+        </Menu>
+
+        <Menu
+          menuButton={
+            <MenuButton className='flex items-center gap-x-2 hover:bg-secondary-100 p-2 rounded-lg transition-colors'>
+              <img
+                src='https://img.freepik.com/foto-gratis/negocio-hombre-negocios-exitoso-trabajando-ordenador-portatil-usando-computadora-sonriendo-pie_1258-26447.jpg'
+                alt=''
+                className='w-6 h-6 object-cover rounded-full'
+              />
+              <span>Jorge Ibarra</span>
+              <RiArrowDownSLine />
+            </MenuButton>
+          }
+          transition
+          align='end'
+          arrow
+          arrowClassName='bg-secondary-100'
+          menuClassName='bg-secondary-100 p-4'
+        >
+          <MenuItem className='p-0 hover:bg-transparent'>
+            <Link
+              to='/profile'
+              className='rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1'
+            >
+              <img
+                src='https://img.freepik.com/foto-gratis/negocio-hombre-negocios-exitoso-trabajando-ordenador-portatil-usando-computadora-sonriendo-pie_1258-26447.jpg'
+                alt=''
+                className='w-8 h-8 object-cover rounded-full'
+              />
+              <div className='flex flex-col text-sm'>
+                <span className='text-sm'>Jorge Ibarra</span>
+                <span className='text-xs text-gray-500'>jorge@gmail.com</span>
+              </div>
+            </Link>
+          </MenuItem>
+          <hr className='my-4 border-gray-500' />
+          <MenuItem className='p-0 hover:bg-transparent'>
+            <Link
+              to='/configuration'
+              className='rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1'
+            >
+              <RiSettings3Line /> Configuration
+            </Link>
+          </MenuItem>
+          <MenuItem className='p-0 hover:bg-transparent'>
+            <Link
+              to='/logout'
+              className='rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1'
+            >
+              <RiLogoutCircleRLine /> Logout
+            </Link>
+          </MenuItem>
+        </Menu>
+      </nav>
+    </header>
+  )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,66 +1,117 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { RiBarChart2Line, RiEarthLine, RiMessage3Line, RiCalendarTodoLine, RiLogoutCircleLine, RiArrowRightSLine, RiMenu3Line, RiCloseLine } from "react-icons/ri";
+/** @format */
+
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import {
+	RiBarChart2Line,
+	RiEarthLine,
+	RiCustomerService2Line,
+	RiCalendarTodoLine,
+	RiLogoutCircleLine,
+	RiArrowRightSLine,
+	RiMenu3Line,
+	RiCloseLine,
+} from "react-icons/ri"
 
 export default function Sidebar() {
-    const [showMenu, setShowMenu] = useState(false)
-    const [showSubmenu, setShowSubmenu] = useState(false)
-    return (
-        <>
-            <div className={`xl:h-[100vh] overflow-y-scroll fixed xl:static  w-[80%] md:w-[40%] lg:w-[30%] xl:w-auto h-full  top-0 bg-secondary-100 p-4 flex flex-col justify-between z-50 ${showMenu ? "left-0" : "-left-full"} transition-all`}>
-                <div>
-                    <h1 className='text-center text-2xl font-bold text-white mb-10'>
-                        Admin<span className='text-primary text-4xl'>.</span>
-                    </h1>
-                    <ul>
-                        <li>
-                            <Link to="/" className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiBarChart2Line className='text-primary' />Analytic</Link>
-                        </li>
-                        <li>
-                            <button onClick={() => setShowSubmenu(!showSubmenu)} className='w-full flex items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
-                                <span className='flex items-center gap-4'>
-                                    <RiEarthLine className='text-primary' />Social media
-                                </span>
-                                <RiArrowRightSLine className={`mt-1 ${showSubmenu && "rotate-90"} transition-all`} />
-                            </button>
-                        </li>
-                        <ul className={`my-2 ${!showSubmenu && "hidden"}`}>
-                            <li>
-                                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white transition-colors'>
-                                    Social media post
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white'>
-                                    Social media stadistic
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white'>
-                                    Social media profile
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white'>
-                                    Social media followers
-                                </Link>
-                            </li>
-                        </ul>
-                        <li>
-                            <Link to="/" className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiMessage3Line className='text-primary' />Messages</Link>
-                        </li>
-                        <li>
-                            <Link to="/" className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiCalendarTodoLine className='text-primary' />Calendar</Link>
-                        </li>
-                    </ul>
-                </div>
-                <nav>
-                    <Link to="/" className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiLogoutCircleLine className='text-primary' />Logout</Link>
-                </nav>
-            </div>
-            <button onClick={() => setShowMenu(!showMenu)} className='xl:hidden fixed bottom-4 right-4 bg-primary text-black p-3 rounded-full z-50'>
-                {showMenu ? <RiCloseLine /> : <RiMenu3Line />}
-            </button>
-        </>
-    )
+	const [showMenu, setShowMenu] = useState(false)
+	const [showSubmenu, setShowSubmenu] = useState(false)
+	return (
+		<>
+			<div
+				className={`xl:h-[100vh] overflow-y-scroll fixed xl:static  w-[80%] md:w-[40%] lg:w-[30%] xl:w-auto h-full  top-0 bg-secondary-100 p-4 flex flex-col justify-between z-50 ${
+					showMenu ? "left-0" : "-left-full"
+				} transition-all`}>
+				<div>
+					<h1 className='text-center text-2xl font-bold text-white mb-10'>
+						Admin<span className='text-primary text-4xl'>.</span>
+					</h1>
+					<ul>
+						<li>
+							<Link
+								to='/'
+								className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
+								<RiBarChart2Line className='text-primary' />
+								Analytic
+							</Link>
+						</li>
+						<li>
+							<button
+								onClick={() => setShowSubmenu(!showSubmenu)}
+								className='w-full flex items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
+								<span className='flex items-center gap-4'>
+									<RiEarthLine className='text-primary' />
+									Social media
+								</span>
+								<RiArrowRightSLine
+									className={`mt-1 ${
+										showSubmenu && "rotate-90"
+									} transition-all`}
+								/>
+							</button>
+						</li>
+						<ul className={`my-2 ${!showSubmenu && "hidden"}`}>
+							<li>
+								<Link
+									to='/'
+									className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white transition-colors'>
+									Social media post
+								</Link>
+							</li>
+							<li>
+								<Link
+									to='/'
+									className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white'>
+									Social media stadistic
+								</Link>
+							</li>
+							<li>
+								<Link
+									to='/'
+									className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white'>
+									Social media profile
+								</Link>
+							</li>
+							<li>
+								<Link
+									to='/'
+									className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white'>
+									Social media followers
+								</Link>
+							</li>
+						</ul>
+						<li>
+							<Link
+								to='/'
+								className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
+								<RiCustomerService2Line className='text-primary' />
+								Technical support
+							</Link>
+						</li>
+						<li>
+							<Link
+								to='/'
+								className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
+								<RiCalendarTodoLine className='text-primary' />
+								Calendar
+							</Link>
+						</li>
+					</ul>
+				</div>
+				<nav>
+					<Link
+						to='/logout'
+						className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
+						<RiLogoutCircleLine className='text-primary' />
+						Logout
+					</Link>
+				</nav>
+			</div>
+			<button
+				onClick={() => setShowMenu(!showMenu)}
+				className='xl:hidden fixed bottom-4 right-4 bg-primary text-black p-3 rounded-full z-50'>
+				{showMenu ? <RiCloseLine /> : <RiMenu3Line />}
+			</button>
+		</>
+	)
 }
